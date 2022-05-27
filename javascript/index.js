a24f1b04c7b57ab211413ed9b78d354687353069
tomas regner <tomasregnerfilippini@gmail.com>
2022-05-26 21:11:17 -0300

    clase 3

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+//a
+
+let ingresaNumero = parseInt(prompt("ingrese Numero"));
+let int = 3;
+let suma = int + ingresaNumero;
+for ( let i = 0; i < 5; i++) {
+    let result = suma + i ;
+    console.log(result);
+    
+}
+
+
+
+//b
+/*let texto = prompt("ingresa ESC")
+
+while (texto != "ESC") {
+    console.log("le erraste")
+    texto = prompt("ingresa ESC")
+
+}
+console.log("ganaste")
+*/
+
+//c
+/*
+let int = prompt("ingrese numero ")
+for (let i = 0; i < int; i++) {
+    
+    console.log("hola");
+    
+}
+*/
+
